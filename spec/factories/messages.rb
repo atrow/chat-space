13846f5bd3c868db8ad7df8b3e38a024aeb3d5b2
atrow <50106451+atrow@users.noreply.github.com>
2019-05-15 21:08:10 +0900

    Message model unit test

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :message do
-    content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/no_image.jpg")}
-    user
-    group
+    body {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    user_id {"1"}
+    group_id {"1"}
+    association :user
+    association :group
   end
 end
