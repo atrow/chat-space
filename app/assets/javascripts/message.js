724c09cad14016cab5d50b2c8309db3b5a0b4993
atrow <50106451+atrow@users.noreply.github.com>
2019-05-25 17:56:53 +0900

    Get newest message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -43,4 +43,24 @@ $(function() {
       $('.main-form__submit-button').prop('disabled', false);
     })
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = Math.max.apply(null,$('.main-content__messages').data('message-id'))
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: 'api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 })
