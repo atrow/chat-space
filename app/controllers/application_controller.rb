76c3cce9055a49a918d68665ad282e2b99cbd6a2
atrow <50106451+atrow@users.noreply.github.com>
2019-06-09 18:23:13 +0900

    BASIC Auth for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
 
   protected
 
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
